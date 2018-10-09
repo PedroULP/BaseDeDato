68d46387ebdcf2a1f40617830be80fb746563c67
PedroULP <32969246+PedroULP@users.noreply.github.com>
2018-10-09 16:12:43 -0300

    Estructura de package

--- app/src/main/java/com/example/pedro/github/Adapters/ProyectoAdapter.java
+++ app/src/main/java/com/example/pedro/github/Adapters/ProyectoAdapter.java
@@ -1,6 +1,16 @@
 package com.example.pedro.github.Adapters;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
 public class ProyectoAdapter {
 
-    
+    private static final String NAME = "Proyecto";
+    private SQLiteDatabase sqlDB;
+
+    public ProyectoAdapter(SQLiteDatabase sqlDB) {
+        this.sqlDB = sqlDB;
+    }
 }
