19641c290d62986bda18b9bfd2c1f4779527aabe
PedroULP <32969246+PedroULP@users.noreply.github.com>
2018-10-09 17:14:06 -0300

    Creada la clase Proyecto

--- app/src/main/java/com/example/pedro/github/Entidades/Proyecto.java
+++ app/src/main/java/com/example/pedro/github/Entidades/Proyecto.java
@@ -1,4 +1,42 @@
 package com.example.pedro.github.Entidades;
 
 public class Proyecto {
+
+    public String descripcion;
+    public double costo;
+    public Cliente cliente_id;
+
+    public Proyecto(String descripcion, double costo, Cliente cliente_id) {
+        this.descripcion = descripcion;
+        this.costo = costo;
+        this.cliente_id = cliente_id;
+    }
+
+    public Proyecto() {}
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public Cliente getCliente_id() {
+        return cliente_id;
+    }
+
+    public void setCliente_id(Cliente cliente_id) {
+        this.cliente_id = cliente_id;
+    }
+
+
 }
