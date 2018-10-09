d5430dbe9dc30ba7b38733375cadf5916d192069
PedroULP <32969246+PedroULP@users.noreply.github.com>
2018-10-09 16:56:11 -0300

    Creada la clase BaseAplication (los errores son debidos a que no se han terminado todas las clases)

--- app/src/main/java/com/example/pedro/github/Aplicattion/BaseAplication.java
+++ app/src/main/java/com/example/pedro/github/Aplicattion/BaseAplication.java
@@ -6,7 +6,42 @@ import android.database.Cursor;
 import com.example.pedro.github.Entidades.Cliente;
 import com.example.pedro.github.Adapters.DBAdapter;
 
-public class BaseAplication {
+import java.util.ArrayList;
+
+public class BaseAplication extends Application{
+
+    DBAdapter dbAdapter;
+
+    @Override
+    public void onCreate() {
+        dbAdapter = new DBAdapter(getApplicationContext());
+        dbAdapter.open();
+
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        dbAdapter.close();
+        super.onTerminate();
+    }
 
 
+    public boolean insertarCliente (Cliente cliente){
+        return dbAdapter.clienteInsert(cliente.getNombre, cliente.getApellido, cliente.getTelefono);
+    }
+
+    public ArrayList<String> nombresClientes () {
+        ArrayList<String> lista = new ArrayList<String>();
+
+        Cursor c = dbAdapter.getDatosCliente();
+        if (c.moveToFirst()){
+            do {
+                lista.add(c.getString(1));
+
+            }   while (c.moveToNext());
+        }
+
+        return  lista;
+    }
 }
