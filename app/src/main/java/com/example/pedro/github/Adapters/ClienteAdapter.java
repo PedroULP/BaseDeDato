acb3f55324fc0257eec35e7427bc2dec26339806
JacostaULP <32969335+JacostaULP@users.noreply.github.com>
2018-10-09 18:01:26 -0300

    Clase DBAdapter creada (no terminada)

--- app/src/main/java/com/example/pedro/github/Adapters/ClienteAdapter.java
+++ app/src/main/java/com/example/pedro/github/Adapters/ClienteAdapter.java
@@ -1,4 +1,79 @@
 package com.example.pedro.github.Adapters;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
 public class ClienteAdapter {
+    private static final String NAME="cliente";
+    private SQLiteDatabase sqlDB;
+
+
+    public ClienteAdapter (SQLiteDatabase sqlDB){
+        this.sqlDB=sqlDB;
+    }
+
+
+    private class Columns implements BaseColumns {
+        public final static String ID="cliente_id";
+        public final static String NOMBRE="nombre";
+        public final static String APELLIDO="apellido";
+        public final static String TELEFONO="telefono";
+
+    }
+
+
+    private final static String[] COLUMNS={
+            Columns.ID,Columns.NOMBRE,Columns.APELLIDO,Columns.TELEFONO};
+
+
+    public final static String CR_TABLE="create table ifnoexists"
+            + NAME + "(" + Columns.ID + "integerprimarykeyautoincrement, "
+            + Columns.NOMBRE + "textonull, " + Columns.APELLIDO + "textonull, "
+            + Columns.TELEFONO + " text)";
+
+
+    public boolean insert(String nombre, String apellido, String telefono) {
+        //Relacionamos campos con valores en el objeto ContentValues.
+        ContentValues values = new ContentValues();
+        values.put(Columns.NOMBRE, nombre);
+        values.put(Columns.APELLIDO, apellido);
+        values.put(Columns.TELEFONO, telefono);
+
+        return sqlDB.insert(NAME, null, values) > 0;
+    }
+
+    public boolean delete (int id){
+        String whereClause = "cliente_id=?";
+        String[] whereArgs = {String.valueOf(id)};
+        return sqlDB.delete(NAME, whereClause, whereArgs)>0;
+    }
+    /**
+     * Devuelve el cursor con todos los nombres registrados en la tabla.
+     *
+     * @return Cursor
+     */
+    public Cursor getNombres(){
+        String[] columns = { Columns.NOMBRE};
+        return sqlDB.query(NAME, columns, null, null, null, null, null);
+    }
+
+    public String getName(){
+        return NAME;
+    }
+
+    public String[] getColumns(){
+        return COLUMNS;
+    }
+
+    public  boolean isEmpty() {
+        return sqlDB.query(NAME, COLUMNS, null, null, null, null, null)
+                .getCount() == 0;
+    }
+
+    public Cursor getDatos() {
+        return sqlDB.query(NAME, COLUMNS, null, null, null, null, null);
+    }
+
 }
